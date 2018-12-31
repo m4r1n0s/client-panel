70943eda3ef181f71711a797a2c7c81fa7bcf124
Marinos <marinos@crowdpolicy.com>
2018-12-31 20:10:07 +0200

    clientDetails and editClient functionality

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import store from './store';
 import AppNavbar from './components/layout/AppNavBar';
 import Dashboard from './components/layout/Dashboard';
 import AddClient from './components/clients/AddClient';
+import EditClient from './components/clients/EditClient';
+import ClientDetails from './components/clients/ClientDetails';
 
 class App extends Component {
   render() {
@@ -19,6 +21,8 @@ class App extends Component {
               <Switch>
                 <Route exact path="/" component={Dashboard} />
                 <Route exact path="/client/add" component={AddClient} />
+                <Route exact path="/client/edit/:id" component={EditClient} />
+                <Route exact path="/client/:id" component={ClientDetails} />
               </Switch>
             </div>
           </div>
