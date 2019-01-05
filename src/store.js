79445381ab00a626895f0b0f603052f093e16f5e
Marinos <marinos@crowdpolicy.com>
2019-01-05 15:49:20 +0200

    alerts

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ import 'firebase/firestore';
 import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
 import { reduxFirestore, firestoreReducer } from 'redux-firestore';
 // Reducers
-// @TODO
+import notifyReducer from './reducers/notifyReducer';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDvtMJp_5boOTukaKTrZRtIM4rT3TAcNIQ',
@@ -36,7 +36,8 @@ const createStoreWithFirebase = compose(
 
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
-  firestore: firestoreReducer
+  firestore: firestoreReducer,
+  notify: notifyReducer
 });
 
 // Create initial state
