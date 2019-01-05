76bdf5b8bac40c6c1ab7186814b05623393d75d4
Marinos <marinos@crowdpolicy.com>
2019-01-06 00:58:13 +0200

    register and protected routes

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -12,18 +12,24 @@ class Login extends Component {
     password: ''
   };
 
+  componentWillMount() {
+    const { allowRegistration } = this.props.settings;
+
+    if (!allowRegistration) {
+      this.props.history.push('/');
+    }
+  }
+
   onSubmit = e => {
     e.preventDefault();
 
     const { firebase, notifyUser } = this.props;
     const { email, password } = this.state;
 
+    // Register with firebase
     firebase
-      .login({
-        email,
-        password
-      })
-      .catch(err => notifyUser('invalid login credentials', 'error'));
+      .createUser({ email, password })
+      .catch(err => notifyUser('That User Already Exists', 'error'));
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
@@ -41,7 +47,7 @@ class Login extends Component {
               ) : null}
               <h1 className="text-center pd-4 pt-3">
                 <span className="text-primary">
-                  <i className="fas fa-lock" /> Login
+                  <i className="fas fa-lock" /> Register
                 </span>
               </h1>
               <form onSubmit={this.onSubmit}>
@@ -69,7 +75,7 @@ class Login extends Component {
                 </div>
                 <input
                   type="submit"
-                  value="Login"
+                  value="Register"
                   className="btn btn-primary btn-block"
                 />
               </form>
@@ -91,7 +97,8 @@ export default compose(
   firebaseConnect(),
   connect(
     (state, props) => ({
-      notify: state.notify
+      notify: state.notify,
+      settings: state.settings
     }),
     { notifyUser }
   )
